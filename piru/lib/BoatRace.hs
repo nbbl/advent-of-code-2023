90ced953aca16deff87dfe9bf9a09507e7c6d709
Pierpaolo Frasa <pfrasa@gmail.com>
2023-12-07 02:24:15 +0100

    Piru: day 6: shorter solution

--- piru/lib/BoatRace.hs
+++ piru/lib/BoatRace.hs
@@ -14,13 +14,6 @@ challengePair = ChallengePair solve1' solve2' testData' challengeData'
 type Input = [Race]
 data Race = Race { _time :: Int, _distance :: Int } deriving (Eq, Show)
 
-data Range
-  = Unbounded
-  | Below Double
-  | Between Double Double
-  | Above Double
-  deriving (Eq, Show)
-
 solve1' :: ByteString -> String
 solve1' = show . product . (solveRace <$>) . readInput1
 
@@ -28,46 +21,20 @@ solve2' :: ByteString -> String
 solve2' = show . solveRace . readInput2
 
 solveRace :: Race -> Int
-solveRace race =
-  length (solveQuadraticGTZeroInts (-1, _time race, -(_distance race)))
+solveRace race = numIntsGTQuad (-1) (_time race) (-(_distance race))
 
-solveQuadraticGTZeroInts :: (Int, Int, Int) -> [Int]
-solveQuadraticGTZeroInts (a, b, c) =
-  (intsInRanges . solveQuadraticGTZero) (fromIntegral a, fromIntegral b, fromIntegral c)
+numIntsGTQuad :: Int -> Int -> Int -> Int
+numIntsGTQuad a b c =
+  numIntsInRange (solveQuadratic (fromIntegral a) (fromIntegral b) (fromIntegral c))
   where
-    intsInRanges :: [Range] -> [Int]
-    intsInRanges [Between x y] =
-      let lower = if fromInteger (ceiling x) == x then ceiling x + 1 else ceiling x
-          upper = if fromInteger (floor y) == y then floor y - 1 else floor y
-      in  [lower..upper]
-    intsInRanges _ = error "impossible or infinite solutions"
+    numIntsInRange :: [Double] -> Int
+    numIntsInRange [x, y] = max 0 $ ceiling y - floor x - 1
+    numIntsInRange _ = error "invalid input"
 
-solveQuadraticGTZero :: (Double, Double, Double) -> [Range]
-solveQuadraticGTZero (a, b, c) = (filter isGTZero . getRanges . solveQuadratic) (a, b, c)
-  where
-    isGTZero :: Range -> Bool
-    isGTZero range = plugIn (rangeTestVal range) > 0
-
-    rangeTestVal :: Range -> Double
-    rangeTestVal Unbounded = 0
-    rangeTestVal (Below x) = x - 1
-    rangeTestVal (Above x) = x + 1
-    rangeTestVal (Between x y) = (x + y) / 2
-
-    plugIn :: Double -> Double
-    plugIn x = a * (x ** 2) + b * x + c
-
-    getRanges :: [Double] -> [Range]
-    getRanges [] = [Unbounded]
-    getRanges [x] = [Below x, Above x]
-    getRanges [x, y] = [Below x, Between x y, Above y]
-    getRanges _ = error "impossible"
-
-solveQuadratic :: (Double, Double, Double) -> [Double]
-solveQuadratic (a, b, c)
-  | discriminant < 0 = []
-  | discriminant == 0 = [(-b) / (2 * a)]
-  | otherwise = sort [(-b - sqrt discriminant) / (2 * a), (-b + sqrt discriminant) / (2 * a)]
+solveQuadratic :: Double -> Double -> Double -> [Double]
+solveQuadratic a b c
+  | discriminant > 0 = sort [(-b - sqrt discriminant) / (2 * a), (-b + sqrt discriminant) / (2 * a)]
+  | otherwise = error "invalid input"
   where
     discriminant = b * b - 4 * a * c
 
