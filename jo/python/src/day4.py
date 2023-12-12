f4386445586f40904f1b1618d22756b1d1d50314
Joanne <joanne254@gmail.com>
2023-12-12 23:25:27 +0100

    Alright, simple unit tests it is then. Day 4 part 1 done.

--- jo/python/src/day4.py
+++ jo/python/src/day4.py
@@ -1,8 +1,7 @@
-from common.solution import Solution, has_tests
+from common.solution import Solution
 
 
 class Day4(Solution):
-    test_mode = True
     sample_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
     Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -11,21 +10,26 @@ class Day4(Solution):
     Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
     input_file = "data/input_day4.txt"
 
-    @has_tests([
-        ('Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
-         [[41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]])
-    ])
     def parse_line(self, line):
         return [[int(n) for n in l.split()]
                 for l in line.strip().split(': ')[1].split(' | ')]
 
+    @staticmethod
+    def get_winning_numbers(winning_numbers, player_numbers):
+        return set(num for num in winning_numbers if num in player_numbers)
+
+    @staticmethod
+    def card_value(card_matches):
+        return 2**(len(card_matches)-1) if card_matches else 0
+
     def run_part1(self, lines, debug=False):
-        pass
+        return sum(self.card_value(self.get_winning_numbers(*self.parse_line(line))) for line in lines)
 
     def run_part2(self, lines, debug=False):
         pass
 
 
 if __name__ == '__main__':
-    day4 = Day4(test_mode=False)
-    day4.parse_line('')
+    day4 = Day4()
+    assert day4.check_sample_solution_part1() == 13
+    print(day4.compute_real_solution_part1())
---
+++ jo/python/test/test_day4.py
@@ -0,0 +1,35 @@
+import unittest
+from parameterized import parameterized
+
+from day4 import Day4
+
+
+class TestDay4(unittest.TestCase):
+
+    @parameterized.expand([
+        ('Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+         [[41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]])
+    ])
+    def test_parse_line(self, line, expected):
+        assert Day4().parse_line(line) == expected
+
+    @parameterized.expand([
+        ([41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53], [48, 83, 17, 86]),
+        ([13, 32, 20, 16, 61], [61, 30, 68, 82, 17, 32, 24, 19], [32, 61]),
+        ([41, 92, 73, 84, 69], [59, 84, 76, 51, 58, 5, 54, 83], [84]),
+        ([31, 18, 13, 56, 72], [74, 77, 10, 23, 35, 67, 36, 11], [])
+    ])
+    def test_get_winning_numbers(self, winning_numbers, player_numbers, expected):
+        res = Day4().get_winning_numbers(winning_numbers, player_numbers)
+        assert set(res) == set(expected)
+
+    @parameterized.expand([
+        ([48, 83, 17, 86], 8),
+        ([], 0)
+    ])
+    def test_card_value(self, card_matches, expected):
+        assert Day4().card_value(card_matches) == expected
+
+
+if __name__ == '__main__':
+    unittest.main()
